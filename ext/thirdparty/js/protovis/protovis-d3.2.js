bbbad93f0f0b0e5da9706da894c057e46093b002
Marcus McLaughlin <marcus@cloudera.com>
2010-10-21 03:34:07 -0700

    Add handling for current offset parent in scrolled containter in pv.Mark.prototype.mouse

--- ext/thirdparty/js/protovis/protovis-d3.2.js
+++ ext/thirdparty/js/protovis/protovis-d3.2.js
@@ -7914,7 +7914,8 @@ pv.Mark.prototype.mouse = function() {
   var x = pv.event.pageX || 0,
       y = pv.event.pageY || 0,
       n = this.root.canvas();
-
+  
+  //console.log("Event--X:" + pv.event.pageX || 0 + " Y:" + pv.event.pageY || 0);
   // Calculate pageX/Y if missing and clientX/Y available
   /*
         CLOUDERA CHANGE:
@@ -7926,9 +7927,20 @@ pv.Mark.prototype.mouse = function() {
     y = pv.event.clientY + (doc && doc.scrollTop  || body && body.scrollTop  || 0) - (doc && doc.clientTop  || body && body.clientTop  || 0);
   }
 
+ 
   do {
-    x -= n.offsetLeft;
-    y -= n.offsetTop;
+    /*  CLOUDERA CHANGE */
+    //Accomodate for the possibility that the current offset parent (n) is in a scrolled object.
+    if (n.scrollLeft != 0) {
+        x -= (n.offsetLeft - n.scrollLeft)
+    } else {
+        x -= n.offsetLeft;
+    }
+    if (n.scrollTop != 0) {
+        y -= (n.offsetTop - n.scrollTop)
+    } else {
+        y -= n.offsetTop;
+    }
   } while (n = n.offsetParent);
 
   /* Compute the inverse transform of all enclosing panels. */
